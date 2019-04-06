fb43bbd482b729d79218e16b83579a6a35eddd7a
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-06 22:13:39 +0300

    Update DHT11.java

--- DHT11.java
+++ DHT11.java
@@ -5,31 +5,29 @@ public class DHT11 {
 
 	private static String line;
 	private static String[] data;
-
-	private static int humidity = 0;
-	private static int temperature = 0;
-
+	static int humidity=0;
+	static int temperature=0;
 	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Runtime rt= Runtime.getRuntime();
+		Process p=rt.exec("python /home/pi/Desktop/dht.py");
+		 BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		 if((line = bri.readLine()) != null){
+			 if(!(line.contains("ERR_CRC") || line.contains("ERR_RNG"))){
 
-		Runtime runtime = Runtime.getRuntime();
-		Process process = runtime.exec("sudo python /home/pi/proiect/licenta/dht11.py");
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-		if ((line = bufferedReader.readLine()) != null) {
-			if (!(line.contains("ERROR"))) {
-
-				data = line.split(" ");
-
-				temperature = Integer.parseInt(data[0]);
-				humidity = Integer.parseInt(data[1]);
-			} else
-				System.out.println("Data Error");
-		}
-
-		bufferedReader.close();
-		process.waitFor();
-		System.out.println("Temperature is : " + temperature + " 'C Humidity is :" + humidity + " %RH");
-		System.out.println("Done.");
+				 data=line.split("ABC");
+			      	System.out.println(data[0]);
+				 temperature=Integer.parseInt(data[0]);
+				 humidity=Integer.parseInt(data[1]);
+			 }
+			 else 
+				 System.out.println("Data Error");
+		 }
+	  
+	      bri.close();
+      	p.waitFor();
+      	System.out.println("Temperature is : "+temperature+" 'C Humidity is :"+ humidity+" %RH");
+      	System.out.println("Done.");
 	}
 
 }
