bdc47decd0d21b503685723ca15eb07ce7750f24
scutaruandrei <scutaru.andrei@gmail.com>
2019-04-07 16:16:26 +0300

    java with python first try

--- dht11.java
+++ dht11.java
@@ -5,30 +5,35 @@ public class dht11 {
 
 	private static String line;
 	private static String[] data;
-	static int humidity=0;
-	static int temperature=0;
+	//static int humidity = 0;
+	static int temperature = 0;
 	
 	public static void main(String[] args) throws Exception {
 		
 		Runtime runtime = Runtime.getRuntime();
-		Process process =runtime.exec("sudo python dht11.py");
+		Process process = runtime.exec("sudo python dht11.py");
 		 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 		 if((line = bufferedReader.readLine()) != null){
 			 
 
-		     //data=line.split("ABC");
-			 System.out.println(line);
-		     //temperature=Integer.parseInt(data[0]);
-			 //humidity=Integer.parseInt(data[1]);
+		     data = line.split("\\n");
+		     temperature = Integer.parseInt(data[0]);
+		     System.out.println(data[0]);
+	             //humidity=Integer.parseInt(data[1]);
+		     //System.out.println(data[1]);
 			 
-			 else {
-				 System.out.println("Data Error");
-		 }
+		 } else {
+			 
+			System.out.println("Data Error");
+			
+			}
 	  
-	      bufferedReader.close();
-      	process.waitFor();
+	bufferedReader.close();
+	process.waitFor();
+	System.out.println("Temperature is : "+temperature);
       	//System.out.println("Temperature is : "+temperature+" 'C Humidity is :"+ humidity+" %RH");
-      	//System.out.println("Done.");
+	System.out.println("Done");
+	
 	}
 
 }
--- dht11.py
+++ dht11.py
@@ -8,8 +8,8 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-	if humidity is not None and temperature is not None:	
-		print temperature, humidity
+	if humidity is not None and temperature is not None:
+		print temperature
 	else:
 		print('Failed to get reading. Try again!')
 	time.sleep(.500)
