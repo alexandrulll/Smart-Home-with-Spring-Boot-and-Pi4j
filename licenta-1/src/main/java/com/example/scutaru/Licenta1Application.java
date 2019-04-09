4b7a835be1da5b89f04db1d2d941d17c43ef3ad2
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-09 09:38:43 +0300

    Add files via upload

---
+++ licenta-1/src/main/java/com/example/scutaru/Licenta1Application.java
@@ -0,0 +1,13 @@
+package com.example.scutaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Licenta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Licenta1Application.class, args);
+	}
+
+}
