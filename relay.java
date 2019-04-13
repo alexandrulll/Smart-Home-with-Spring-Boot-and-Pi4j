d8739ffe5b7c003b101845d9d4f6b4827576273b
alexandrulll <45544001+alexandrulll@users.noreply.github.com>
2019-04-13 10:34:08 +0300

    Update relay.java

--- relay.java
+++ relay.java
@@ -1,4 +1,12 @@
-public class relay {
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.util.CommandArgumentParser;
+
+class relay {
 	
 public static void main(String [] args) {
 	
